9891cf09677a1dea65de802b9dc3e232c25daedd
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-12-11 20:18:11 +0530

    introduce debugging; make repository delimiting more consistent

--- subtree.sh
+++ subtree.sh
@@ -1,20 +1,40 @@
 CURRENT=`pwd`
 FILENAME="$CURRENT/final_out_`date +%s`"
-touch $FILENAME
-echo "Output goes to $FILENAME"
-echo
-echo
 
-if [ $# -ge 1 ]; then
-  echo "Passed first argument"
-  echo $1;
+touch $FILENAME
+
+DEBUG="YES"
+DEBUG=""
+
+function debug {
+  if [[ $DEBUG == "YES" ]]; then
+    echo $@;
+  fi
+}
+
+# if [ $# -ge 1 ]; then
+  # echo "Passed first argument"
+  # echo $1;
+  # cd $1;
+# fi;
+
+if [ -n "$1" ]; then
   cd $1;
 fi;
 
+BEGINNING="yesterday"
+if [ -n "$2" ]; then
+  BEGINNING="$2"
+fi
+
 echo "Found `ls | wc -l` files under `pwd`"
+echo "Searching for commits from $BEGINNING"
+echo "Output goes to $FILENAME.final"
+echo
+echo
 
 for dir in `ls`; do
-  echo "Searching for $dir in `pwd`";
+  debug "Searching for $dir in `pwd`";
 
   # check if this is a directory
   if [ -d "$dir" ]; then
@@ -26,24 +46,24 @@ for dir in `ls`; do
 
       cd $dir
       # check if this is a directory that is a git repository
-      echo "  Entered `pwd`";
+      debug "  Entered `pwd`";
       if [ -e .git ]; then
 
-        echo "    Running in `pwd`"
-        git --no-pager log --since="yesterday" \
+        debug "    Running in `pwd`"
+        git --no-pager log --since="$BEGINNING" \
           --date=short \
           --pretty=format:"$(git config --get remote.origin.url | awk -F '/' '{ print $2 }' | awk -F '.' '{ print $1 }'): %ad - [%s]" --author="$(git config --get user.name)" >> $FILENAME 
-        echo "" >> $FILENAME
+        printf "\n\n" >> $FILENAME
       else
-        echo "      $dir isn't a git repo"
+        debug "      $dir isn't a git repo"
       fi;
       cd ..;
     else
-      echo "  cd into $dir didn't work out!"
-      echo "ERROR: $RESULT";
+      debug "  cd into $dir didn't work out!"
+      debug "ERROR: $RESULT";
     fi;
   else
-    echo "  $dir is not a directory";
+    debug "  $dir is not a directory";
   fi;
 done;
 
